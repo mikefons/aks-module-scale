6b832b9b9f610213dc65a37656689156a414bde1
mikefons <fonseca.michael@gmail.com>
2021-09-10 20:43:18 -0400

    added vars

--- main.tf
+++ main.tf
@@ -2,4 +2,6 @@ module "aks-cluster" {
   source  = "app.terraform.io/mfonseca-org/aks-cluster/azurerm"
   version = "1.0.0"
   # insert required variables here
+  appId = var.appId
+  password = var.password
 }
